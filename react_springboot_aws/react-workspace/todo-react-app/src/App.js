576d244bcd75fffda10a2cf73b9cdeecae198a45
ChanYeongGwak <maxelto123@gmail.com>
2022-07-25 00:11:02 +0900

    frontend dev project setting

---
+++ react_springboot_aws/react-workspace/todo-react-app/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div></div>; // JSX
+};
+
+export default App;
---
+++ react_springboot_aws/react-workspace/todo-react-app/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // 루트 엘리먼트 지정
+// 루트 엘리먼트에 컴포넌트 추가
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
