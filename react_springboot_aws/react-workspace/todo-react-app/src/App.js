668b680906d32921663b3fe27fc3b4b887362410
ChanYeongGwak <maxelto123@gmail.com>
2022-07-26 00:07:24 +0900

    frontend dev reactjs

--- react_springboot_aws/react-workspace/todo-react-app/src/App.js
+++ react_springboot_aws/react-workspace/todo-react-app/src/App.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import Todo from "./Todo";
+import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@mui/material";
+import "./App.css";
 
-const App = () => {
-  return <div></div>; // JSX
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { id: "0", title: "Hello World 1", done: true },
+        { id: "1", title: "Hello World 2", done: false },
+      ],
+    };
+  }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items : ", this.state.items);
+  };
+
+  delete = (item) => {
+    const thisItems = this.state.items; // 기존 데이터
+    console.log("Before Update Items : ", this.state.items);
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      console.log("Update Items : ", this.state.items);
+    });
+  };
+
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete} />
+          ))}
+        </List>
+      </Paper>
+    );
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ react_springboot_aws/react-workspace/todo-react-app/src/Todo.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import {
+  ListItem,
+  ListItemText,
+  InputBase,
+  Checkbox,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@mui/material";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: props.item, readOnly: true };
+    this.delete = props.delete;
+  }
+
+  deleteEventHandler = () => {
+    this.delete(this.state.item);
+  };
+
+  offReadOnlyMode = () => {
+    console.log("Event! ", this.state.readOnly);
+    this.setState({ readOnly: false }, () => {
+      console.log("ReadOnly? ", this.state.readOnly);
+    });
+  };
+
+  enterKeyEventHandler = (e) => {
+    if (e.key === "Enter") {
+      this.setState({ readOnly: true });
+    }
+  };
+
+  editEventHandler = (e) => {
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({ item: thisItem });
+  };
+
+  checkboxEventHandler = (e) => {
+    const thisItem = this.state.item; // 기존 데이터 유지
+    thisItem.done = !thisItem.done;
+    this.setState({ item: thisItem });
+  };
+
+  render() {
+    const item = this.state.item;
+
+    return (
+      <ListItem>
+        <Checkbox checked={item.done} onChange={this.checkboxEventHandler} />
+        <ListItemText>
+          <InputBase
+            inputProps={{
+              "aria-label": "naked",
+              readOnly: this.state.readOnly,
+            }}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+            onClick={this.offReadOnlyMode}
+            onKeyPress={this.enterKeyEventHandler}
+            onChange={this.editEventHandler}
+          />
+        </ListItemText>
+
+        <ListItemSecondaryAction>
+          <IconButton
+            aria-label="Delete Todo"
+            onClick={this.deleteEventHandler}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  }
+}
+
+export default Todo;
