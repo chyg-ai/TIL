12998cf4aa66f08042a590beb75083d604684cb9
ChanYeongGwak <maxelto123@gmail.com>
2022-08-05 09:01:11 +0900

    react chap21

---
+++ modern_javascript_deep_dive/chap21/ex_21_09.js
@@ -0,0 +1,5 @@
+console.log(globalThis.parseInt("F", 16));
+
+console.log(parseInt("F", 16));
+
+console.log(globalThis.parseInt === parseInt);
---
+++ modern_javascript_deep_dive/chap21/ex_21_12.js
@@ -0,0 +1,7 @@
+console.log(globalThis.Infinity === Infinity);
+
+console.log(3 / 0);
+
+console.log(-3 / 0);
+
+console.log(typeof Infinity);
