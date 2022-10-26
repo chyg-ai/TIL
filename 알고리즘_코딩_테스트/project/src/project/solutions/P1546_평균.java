46cf9a9ad03406af7c833324f4407645f417f46c
chanyeong <maxelto123@gmail.com>
2022-10-26 22:26:12 +0900

    알고리즘 코딩 테스트

--- 알고리즘_코딩_테스트/project/src/project/solutions/P1546_평균.java
+++ 알고리즘_코딩_테스트/project/src/project/solutions/P1546_평균.java
@@ -1,28 +1,24 @@
-package project.main;
+package project.solutions;
 
 import java.util.Scanner;
 
-public class Main {
+public class P1546_평균 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int N=sc.nextInt();
-		int[] A=new int[N];
-		long max=0;
+		int A[]=new int[N];
 		for(int i=0; i<N; i++) {
 			A[i]=sc.nextInt();
-			if(max<A[i]) {
-				max=A[i];
-			}
 		}
-				
 		long sum=0;
+		long max=0;
 		for(int i=0; i<N; i++) {
-			sum+=A[i];
+			if(A[i]>max)max=A[i];
+			sum=sum+A[i];
 		}
 		
 		System.out.println(sum*100.0/max/N);
-		
 	}
 
 }
