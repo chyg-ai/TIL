2254564596a081483e3dbe5dfc33ca753e3879fb
chanyeong <maxelto123@gmail.com>
2022-11-08 23:15:30 +0900

    [#3] 스프링부트_핵심_가이드

---
+++ spring_boot_course/chap06/jpa/src/main/java/com/springboot/jpa/service/impl/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.springboot.jpa.service.impl;
+
+import com.springboot.jpa.data.dao.ProductDAO;
+import com.springboot.jpa.data.dto.ProductDto;
+import com.springboot.jpa.data.dto.ProductResponseDto;
+import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductDAO productDAO;
+
+    @Autowired
+    public ProductServiceImpl(ProductDAO productDAO){
+        this.productDAO=productDAO;
+    }
+
+    @Override
+    public ProductResponseDto getProduct(Long number){
+        return null;
+    }
+
+    @Override
+    public ProductResponseDto saveProduct(ProductDto productDto){
+        return null;
+    }
+
+    @Override
+    public ProductResponseDto changeProductName(Long number, String name) throws Exception{
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {}
+}
